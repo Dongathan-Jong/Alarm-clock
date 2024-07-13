19915236c97a8357ace3bb7fa73470735b4e3a73
Dongathan-Jong <duclingsyt@gmail.com>
2024-07-13 11:16:18 -0400

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,38 +1,53 @@
+/*
+Author:        Jonathan Dong
 
+Name:          Alarm Clock
+
+Function:      This code is designed for four 7 seg displays 
+               connected via CD4511 chips to display an alarm
+               clock, attached to 3 buttons and a buzzer. 
+*/
+
+// define pins for right most digit
 #define digit11 A5
 #define digit12 A4
 #define digit13 0
 #define digit14 1
 
+// define pins for right middle digit
 #define digit21 2
 #define digit22 3
 #define digit23 4
 #define digit24 5
 
+// define pins for left middle digit
 #define digit31 6
 #define digit32 7
 #define digit33 8
 #define digit34 9
 
+// define pins for left most digit
 #define digit41 10
 #define digit42 11
 #define digit43 12
 #define digit44 13
 
+// define pins for buttons and buzzer
 #define leftButton A1
 #define middleButton A2
 #define rightButton A3
 #define buzzer A0
 
-int minutes = 12;
-int seconds = 30;
+// create and initiate variables for minutes, seconds, and hours
+int minutes = 0;
+int seconds = 0;
 int minutesTens = 0;
 int minutesOnes = 0;
 int hours = 12;
 int hoursTens = 1;
 int hoursOnes = 2;
-int time = 0000;
 
+// create and initiate variables for the time of alarm
 int alarmMinute = 0;
 int alarmMinuteOnes = 0;
 int alarmMinuteTens = 0;
@@ -40,31 +55,43 @@ int alarmHour = 0;
 int alarmHourOnes = 0;
 int alarmHourTens = 0;
 
+// create and intiate boolean variables
 boolean middleButtonPressed = false;
 boolean copyTime = false;
 
+/* 
+Function: setup()
+
+Use:      Set up all pins used and starting time
+*/
+
 void setup()
 {
+  // set up pins for right most digit
   pinMode(digit11, OUTPUT);
   pinMode(digit12, OUTPUT);
   pinMode(digit13, OUTPUT);
   pinMode(digit14, OUTPUT);
   
+  // set up pins for right middle digit
   pinMode(digit21, OUTPUT);
   pinMode(digit22, OUTPUT);
   pinMode(digit23, OUTPUT);
   pinMode(digit24, OUTPUT);
   
+  // set up pins for left middle digit
   pinMode(digit31, OUTPUT);
   pinMode(digit32, OUTPUT);
   pinMode(digit33, OUTPUT);
   pinMode(digit34, OUTPUT);
  
+  // set up pins for left most digit
   pinMode(digit41, OUTPUT);
   pinMode(digit42, OUTPUT);
   pinMode(digit43, OUTPUT);
   pinMode(digit44, OUTPUT);
   
+  // set up pins for buttons and buzzer
   pinMode(leftButton, INPUT);
   pinMode(middleButton, INPUT);
   pinMode(rightButton, INPUT);
@@ -74,13 +101,21 @@ void setup()
   digitalWrite(digit41,HIGH);
 }
 
+/*
+Function: loop()
+
+Use:      Main loop of program
+*/
 
 void loop()
 {
+  // check if the clock is in alarm mode
   if(middleButtonPressed == false)
   {
+    // check if the button is pressed
     if(digitalRead(middleButton) == 1)
     {
+      // set clock to alarm mode and beep to tell user
       middleButtonPressed = true;
       copyTime = true;
       tone(buzzer,440,100);
@@ -88,10 +123,13 @@ void loop()
     } 
   }
   
+  // check if clock is in alarm mode
   if(middleButtonPressed)
   {
+    // check if middle button is held
     if(digitalRead(middleButton) == 1)
     {
+      // set clock out of alarm mode and beep to indicate
        middleButtonPressed = false;
        tone(buzzer,440,100);
        delay(5000);
@@ -99,67 +137,89 @@ void loop()
     }
   }
  
+  // check if clock is in alarm mode
   if(middleButtonPressed)
   {
+    // call the alarm mode method
     alarmMode();
   }
  
+  // check if the middle button is not pressed
   if(middleButtonPressed == false)
-  {if(digitalRead(leftButton) == 1)
-    {
-      hours++;
-      delay(100);
-    }
+  {
+    // increment the minutes if right button is held
     if(digitalRead(rightButton) == 1)
 	{
       minutes++;
 	  delay(100);
+    }
     
-    
+    // increment the hours if left button is held
+    if(digitalRead(leftButton) == 1)
+    {
+      hours++;
+      delay(100);
+    }
   }
-
-	if(seconds == 60)
+  
+  // check if the seconds counter hits 60 and reset 
+  if(seconds == 60)
   {
+    // reset seconds and add a minute
     seconds = 0;
     minutes++;
   }
+  
+  // check if the minutes counter hits 60 and reset
+  if(minutes == 60)
+  {
+    // reset minutes and add an hour
+    minutes = 0;
+    hours++;
+  }
+  
+  // check if the hours counter hits 24 and reset
   if(hours == 24)
   {
+    // set all hour values to 0
    	hours = 0; 
     hoursOnes = 0;
     hoursTens = 0;
   }
-  if(minutes == 60)
+  
+  
+  // increment up every minute for clock to function
+  if(middleButtonPressed == false)
   {
-    minutes = 0;
-    hours--;
+    //CHANGE THIS VALUE TO CHANGE HOW FAST ONE SECOND IS
+    //ONE SECOND = 1000
+    delay(1000);
+  	seconds++;
   }
   
   
+  // get the ones and tens values of the minutes
   minutesOnes = minutes % 10;
   minutesTens = minutes / 10;
   
-  if(middleButtonPressed == false)
-  {
-    delay(1012);
-  	seconds++;
-  }
-
-    minutesOnes = minutes % 10;
-  minutesTens = minutes / 10;
-  
+  // get the ones and tens values of the hours
   hoursOnes = hours % 10;
   hoursTens = hours / 10;
   
+  // check if clock is not in alarm mode
   if(middleButtonPressed == false)
   {
+    // update the time displayed
     updateTime();
   }
   
+  // if the time lines up with the alarm time sound the alarm
   if(minutes == alarmMinute && hours == alarmHour && middleButtonPressed == false)
   {
+    // wait for user to press the right button
     while(digitalRead(rightButton) == 0)
     {
+      // beep until pressed
       tone(buzzer, 440,250);
       delay(500);
       
@@ -167,12 +227,20 @@ void loop()
   }
 }
 
+/*
+Function: updateTime()
+
+Use:      This method will display the time on the four 
+		  7seg displays through the variables minutes and hours.
+*/
 
 void updateTime()
 {
+  // set the digit in the ones place for minutes
   switch(minutesOnes)
   {
     case 0:
+    	// display 0
     	digitalWrite(digit11,LOW);
     	digitalWrite(digit12,LOW);
     	digitalWrite(digit13,LOW);
@@ -258,6 +326,8 @@ void updateTime()
     	break;
   }
   
+  // check which number is the tens value for minutes
+  // display the number
   switch(minutesTens)
   {
     case 0:
@@ -315,6 +385,8 @@ void updateTime()
     	digitalWrite(digit23,HIGH);
     	break;
   }
+  // check which number is the ones value for hours
+  // display the number
   switch(hoursOnes)
   {
     case 0:
@@ -404,6 +476,8 @@ void updateTime()
     	break;
   }
   
+  // check which number is the tens value for hours
+  // display the number
   switch(hoursTens)
   {
     case 0:
@@ -423,10 +497,16 @@ void updateTime()
     	break;
   }
 }
+  
+/*
+Function: alarmMode()
 
-
+Use:      The clock will enter this method when the user is 
+          setting the alarm time
+*/
 void alarmMode()
 {
+  // Copy the current time for the alarm to start
   if(copyTime)
   {
     alarmMinute = minutes;
@@ -434,24 +514,29 @@ void alarmMode()
     copyTime = false;
   }
     
+  // get the ones and tens values of the minutes
   alarmMinuteOnes = alarmMinute % 10;
   alarmMinuteTens = alarmMinute / 10;
   	
+  // get the ones and tens values of the hours
   alarmHourOnes = alarmHour % 10;
   alarmHourTens = alarmHour / 10;
      
+  // add minutes to the alarm when the right button is pressed
   if(digitalRead(rightButton) == 1)
   {
     alarmMinute++;
     delay(100);
   }
   
+  // add hours to the alarm when the left button is pressed
   if(digitalRead(leftButton) == 1)
   {
     alarmHour++;
     delay(100);
   }
   
+  // reset the clock when the user sets 24 hours
   if(alarmHour == 24)
   {
     // set hours to 0
@@ -460,6 +545,7 @@ void alarmMode()
     alarmHourTens = 0;
   }
   
+  // reset the minutes when the user sets higher than 60 minutes and add an hour
   if(alarmMinute == 60)
   {
     // set hours to 0
@@ -468,6 +554,8 @@ void alarmMode()
   
  
   
+  // check which number is the ones value for minutes
+  // display the number
   switch(alarmMinuteOnes)
   {
     case 0:
@@ -525,6 +613,8 @@ void alarmMode()
     	digitalWrite(digit14,HIGH);
     	break;
   }
+  // check which number is the tens value for minutes
+  // display the number
   switch(alarmMinuteTens)
   {
     case 0:
@@ -565,6 +655,8 @@ void alarmMode()
     	digitalWrite(digit21,LOW);
     	break;
   }
+  // check which number is the ones value for hours
+  // display the number
   switch(alarmHourOnes)
   {
     case 0:
@@ -624,6 +716,8 @@ void alarmMode()
     	break;
   }
   
+  // check which number is the tens value for hours
+  // display the number
   switch(alarmHourTens)
   {
     case 0:
